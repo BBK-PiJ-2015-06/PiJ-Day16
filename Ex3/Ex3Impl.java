5599350c4e2ce3bbd0440659bfef559acea6dbfb
unknown <gshian01@CSPC114.dcs.bbk.ac.uk>
2015-11-18 15:58:54 +0000

    Solution to exercise 3 cat completed Method now also prints contents of files that aren't directories.

--- Ex3/Ex3Impl.java
+++ Ex3/Ex3Impl.java
@@ -5,10 +5,23 @@ public class Ex3Impl implements Ex3 {
 	public void showContents(String name) {
 		try {
 			File myFile = new File("." + File.separator + name);
-			if(myFile.exists() && myFile.isDirectory()) {
-				String[] myFiles = myFile.list();
-				for(int index = 0; index < myFiles.length; index++) {
-					System.out.println(myFiles[index]);
+			if(myFile.exists()) {
+				if(myFile.isDirectory()) {
+					String[] myFiles = myFile.list();
+					for(int index = 0; index < myFiles.length; index++) {
+						System.out.println(myFiles[index]);
+					}
+				} else {
+					try (BufferedReader in = new BufferedReader(new FileReader(myFile))) {
+						String line;
+						while((line = in.readLine()) != null) {
+							System.out.println(line);
+						}
+					} catch (FileNotFoundException ex) {
+						ex.printStackTrace();
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
 				}
 			} else {
 				System.out.println("FILE NOT FOUND");
